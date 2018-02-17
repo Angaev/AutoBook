975320352e68e1cfebc910b40b9579e155e9c18c
Artem Angaev <ololok@mail.ua>
2018-02-17 18:25:21 +0300

    Переделал функцию getAllLikedBooks
    #9
    стало на 1 join меньше и на 1 подзапрос больше

--- include/bookDB.inc.php
+++ include/bookDB.inc.php
@@ -288,13 +288,13 @@
               IFNULL(books.image, "img/book_cover/no_cover.png") as image,
               (SELECT COUNT(DISTINCT id) FROM likes WHERE likes.book_id = books.id AND likes.actual = 1) AS likeCount, 
               (SELECT COUNT(DISTINCT id) FROM comments WHERE comments.book_id = books.id AND comments.actual = 1) AS commentCount 
-              FROM books      
+              FROM books
               LEFT JOIN publishing_houses ON books.publishing_house_id = publishing_houses.id  
-              LEFT JOIN likes ON likes.book_id = books.id 
               WHERE 
-                likes.user_id = "' . dbQuote($userId) . '"
-                AND
-                likes.actual = 1
+                books.id IN 
+                (
+                  SELECT book_id FROM likes WHERE likes.user_id = "' . dbQuote($userId) . '" AND likes.actual = 1
+                )
               LIMIT ' . dbQuote($start) . ', ' . dbQuote(LIMIT_ON_PAGE);
 
         return dbQueryGetResult($query);
