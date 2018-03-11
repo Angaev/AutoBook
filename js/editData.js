7b7fd9cd57d8746212398c41f9261b54f310f642
Artem Angaev <ololok@mail.ua>
2018-03-11 14:34:03 +0300

    upgrade

--- js/editData.js
+++ js/editData.js
@@ -1,7 +1,5 @@
 
 $(document).ready ( function() {
-  var NO_LIKE_PRESSED = 1;
-  var LIKE_PRESSED = 2;
   
   $("#fileChoice").change(function() {
       console.log('Done');
@@ -13,9 +11,6 @@ $(document).ready ( function() {
         type: "POST",
         dataType : "json",
         data: ({}),
-        beforeSend: function() {
-          console.log("Отправка");
-        },
         success: function(data) {
             $("#avatar").attr("src", data);
             $("#deleteAvatar").text("Удалено");
